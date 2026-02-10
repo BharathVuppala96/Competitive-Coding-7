329e1e5a2d7d581ef57e0476142b2ede09c6b2ed
BharathVuppala96 <bharathv8696@gmail.com>
2026-02-10 15:51:00 -0800

    2problems completed

---
+++ meeting rooms II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        heap=[]
+        intervals.sort(key=lambda x:x[0])
+
+        heapq.heappush(heap,intervals[0][1])
+
+        for start, end in intervals[1:]:
+            if start>=heap[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap,end)
+        
+        return len(heap)
+            
